72b25698c5747f3236e6e9f68aaf30faacbba3ee
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-11-04 19:50:29 -0500

    chore: upgrade dependencies

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
 const bucket = new aws.s3.Bucket("my-bucket", {
-  website: {
-    indexDocument: "index.html",
-  }
+	website: {
+		indexDocument: "index.html",
+	},
 });
 new aws.s3.BucketOwnershipControls("ownership-controls", {
-  bucket: bucket.id,
-  rule: {
-      objectOwnership: "ObjectWriter"
-  }
+	bucket: bucket.id,
+	rule: {
+		objectOwnership: "ObjectWriter",
+	},
 });
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
-  bucket: bucket.id,
-  blockPublicAcls: false,
-});
-new aws.s3.BucketObject("index.html", {
-  bucket: bucket.id,
-  source: new pulumi.asset.FileAsset("./index.html"),
-  contentType: "text/html",
-  acl: "public-read",
-}, { dependsOn: publicAccessBlock });
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
+	"public-access-block",
+	{
+		bucket: bucket.id,
+		blockPublicAcls: false,
+	},
+);
+new aws.s3.BucketObject(
+	"index.html",
+	{
+		bucket: bucket.id,
+		source: new pulumi.asset.FileAsset("./index.html"),
+		contentType: "text/html",
+		acl: "public-read",
+	},
+	{ dependsOn: publicAccessBlock },
+);
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
